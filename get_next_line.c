35c086ea2487ad9510263cf13d54e29e246f4cc9
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2021-10-21 23:07:59 +0200

    Basic final version !

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,93 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 16:08:45 by ycornamu          #+#    #+#             */
-/*   Updated: 2021/10/20 19:06:58 by ycornamu         ###   ########.fr       */
+/*   Updated: 2021/10/21 22:05:40 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
-
-char	*ft_strjoinbuf(char *s1, char s2[BUFFER_SIZE], const size_t s2_size);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlen(const char *s);
+#include <stdlib.h>
 
 char	*get_next_line(int fd)
 {
 	static char	*readed;
+	static int	eof;
 	size_t		nb_readed;
-	size_t		line_size;
-	char		buf[BUFFER_SIZE];
-	char		*tmp;
 
+	if (read(fd, 0, 0))
+		return (clean(&readed));
 	if (readed == NULL)
-		readed = ft_calloc(1, sizeof(char));
-	while (read(fd, NULL, 0) >= 0)
 	{
-		nb_readed = read(fd, buf, BUFFER_SIZE);
-		readed = ft_strjoinbuf(readed, buf, nb_readed);
-		if (! readed)
-			return (NULL);
-		if (ft_strchr(readed, '\n') != NULL)
-			break ;
+		readed = ft_calloc(1, sizeof(char));
+		eof = 0;
 	}
-	line_size = ft_strchr(readed, '\n') - readed;
-	tmp = ft_substr(readed, 0, line_size);
-	readed = ft_substr(readed, line_size, ft_strlen(readed) - line_size);
-	if (! tmp || ! readed)
-		return (NULL);
-	return (tmp);
+	if (ft_strchr(readed, '\n'))
+		return (return_line(&readed));
+	else if (! eof)
+	{
+		nb_readed = read_line(fd, &readed);
+		if (nb_readed > 0 && *readed)
+			return (return_line(&readed));
+		else if (! nb_readed)
+			eof = 1;
+	}
+	if (eof && *readed)
+		return (return_line(&readed));
+	return (clean(&readed));
+}
+
+int	read_line(int fd, char **readed)
+{
+	char	buf[BUFFER_SIZE];
+	int		total_chars;
+	int		total_buf;
+	int		eof;
+
+	total_chars = 0;
+	total_buf = 0;
+	eof = 0;
+	while (! eof && ! ft_strchr(*readed, '\n'))
+	{
+		total_buf = read(fd, buf, BUFFER_SIZE);
+		if (total_buf < 0)
+			return (-1);
+		else if (total_buf == 0)
+			eof = 1;
+		else
+		{
+			*readed = ft_strjoinbuf(*readed, buf, total_buf);
+			total_chars += total_buf;
+		}
+	}
+	return (total_chars);
+}
+
+char	*return_line(char **readed)
+{
+	char	*out;
+	char	*tmp;
+	size_t	line_size;
+
+	if (ft_strchr(*readed, '\n'))
+		line_size = ft_strchr(*readed, '\n') - *readed + 1;
+	else
+		line_size = ft_strlen(*readed);
+	out = ft_substr(*readed, 0, line_size);
+	if (! out)
+		return (clean(readed));
+	tmp = ft_substr(*readed, line_size, ft_strlen(*readed) - line_size);
+	free(*readed);
+	if (! tmp)
+		return (clean(&out));
+	*readed = tmp;
+	return (out);
+}
+
+char	*clean(char **s)
+{
+	if (*s)
+		free(*s);
+	*s = NULL;
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,28 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 16:09:27 by ycornamu          #+#    #+#             */
-/*   Updated: 2021/10/20 18:47:54 by ycornamu         ###   ########.fr       */
+/*   Updated: 2021/10/21 23:01:29 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
+# include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoinbuf(char *s1, char s2[BUFFER_SIZE], const size_t s2_size);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+int		read_line(int fd, char **readed);
+char	*return_line(char **readed);
+char	*clean(char **s);
 char	*get_next_line(int fd);
 
 #endif
